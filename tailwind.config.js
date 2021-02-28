cc19c9340c6f978cd2254c91ed0d2b7bb75d864a
Danial Hasan <danialhasan14@gmail.com>
2021-02-28 17:32:47 -0500

    Removed tailwind.config.js's purge array of views, attempt 1 at fixing 'no-css' problem during deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   purge: [
-    '/views/404.ejs',
-    '/views/dashboard.ejs',
-    '/views/index.ejs',
-    '/views/layout.ejs',
-    '/views/users.ejs',
+    // '/views/404.ejs',
+    // '/views/dashboard.ejs',
+    // '/views/index.ejs',
+    // '/views/layout.ejs',
+    // '/views/users.ejs',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
