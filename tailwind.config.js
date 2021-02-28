00f4f0df2461e6f3062a86469332dacf848bf05c
Danial Hasan <danialhasan14@gmail.com>
2021-02-28 18:06:21 -0500

    Tailwind purge test 2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   purge: [
     // '/views/404.ejs',
-    '/dashboard.ejs',
-    'index',
+    'dashboard',
+    'index.ejs',
     // '/views/layout.ejs',
     // '/views/users.ejs',
   ],
