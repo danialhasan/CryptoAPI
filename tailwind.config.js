0b7179233176d21826c01faa04f891dd59fec341
Danial Hasan <danialhasan14@gmail.com>
2021-04-16 03:25:49 -0400

    added tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-  purge: {
-    // '/views/404.ejs',
-    enabled: true,
-    content: [
-      'views/dashboard.ejs',
-      'views/index.ejs',
-      'views/layout.ejs',
-      // '/views/users.ejs',
-    ]
-  },
+  purge: [
+    'views/*.ejs'
+    // '/views/users.ejs',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
